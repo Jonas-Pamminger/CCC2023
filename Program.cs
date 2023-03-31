e274a8ddf28dd4ce7efad1b9ddc5ad0a034f9fbd
Jonas Pamminger <j.pamminger@HTBLALeonding.onmicrosoft.com>
2023-03-31 07:35:30 +0200

    gitignore

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Hello World! program
+namespace CCCCCC
+{
+    class MainClass {         
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Run(File.ReadAllLines($"input-{i}.txt"), i);
+            }
+        }
+
+        static void Run(string[] input, int time)
+        {
+            string[] result = null;
+            
+            File.WriteAllLines($"output-{time}.txt", result);
+        }
+    }
+}
